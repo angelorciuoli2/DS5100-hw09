cc5c746a0dc5e241a47e042a00fa81e9e1379ec6
Angelo Orciuoli <arorciuoli@gmail.com>
2025-03-22 15:11:42 -0400

    Set up package structure and added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='booklover',
+    version='0.1',
+    description='A package for tracking books read and ratings',
+    url='https://github.com/angelorciuoli2/DS5100-hw09',
+    author='Angelo Orciuoli',
+    author_email='arorciuoli@gmail.com',
+    license='MIT',
+    packages=['booklover']
+)
+
